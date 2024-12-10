8504c92bcb451e942cf3307e01349c0e948badd5
ZhuchkovAA <jaa2003@mail.ru>
2024-12-10 19:57:19 +0300

    Добавление админа

--- Zhuchkov_backend/Zhuchkov_backend/Controllers/UsersController.cs
+++ Zhuchkov_backend/Zhuchkov_backend/Controllers/UsersController.cs
@@ -138,6 +138,7 @@ namespace Zhuchkov_backend.Controllers
             public string? TagTelegram { get; set; }
             public string? FirstName { get; set; }
             public string? LastName { get; set; }
+            public bool? IsAdmin { get; set; }
             public bool? IsActive { get; set; }
             public string? Password { get; set; }
         }
@@ -159,6 +160,9 @@ namespace Zhuchkov_backend.Controllers
 
             if (request.LastName != null)
                 user.LastName = request.LastName;
+
+            if (request.IsAdmin.HasValue)
+                user.IsAdmin = request.IsAdmin.Value;
 
             if (request.IsActive.HasValue)
                 user.IsActive = request.IsActive.Value;
