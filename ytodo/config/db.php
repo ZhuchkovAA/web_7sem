41dd25cd6421637144f1ee21df0a64bc9de2e5d9
ZhuchkovAA <jaa2003@mail.ru>
2024-12-11 21:22:18 +0300

    yii to do

---
+++ ytodo/config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mariadb-container;dbname=todo_app',
+    'username' => 'root',
+    'password' => 'rootPasswordMariaDB',
+    'charset' => 'utf8',
+];
+
